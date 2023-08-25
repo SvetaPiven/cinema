a4256507ecc387811a1563a6d976e618f6745572
Sveta Piven <svetapiven93@icloud.com>
2023-08-25 12:28:11 +0300

    bug fix

--- src/main/java/org/example/entity/Category.java
+++ src/main/java/org/example/entity/Category.java
@@ -1,10 +1,15 @@
 package org.example.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.Objects;
 
-@Data
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
 public class Category {
 
     private Long id;
@@ -12,4 +17,61 @@ public class Category {
     private String name;
 
     private List<Film> films;
+
+    public Category() {
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+
+    public Category(Long id, String name, List<Film> films) {
+        this.id = id;
+        this.name = name;
+        this.films = films;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFilms(List<Film> films) {
+        this.films = films;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id) && Objects.equals(name, category.name) && Objects.equals(films, category.films);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, films);
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", films=" + films +
+                '}';
+    }
 }
