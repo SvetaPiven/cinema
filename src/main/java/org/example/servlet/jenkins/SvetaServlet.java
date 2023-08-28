c33209bda83a1788a78cec906dfab4aa13e90e4e
Sveta Piven <svetapiven93@icloud.com>
2023-08-28 20:36:50 +0300

    feat: added test servlet (jenkins)

---
+++ src/main/java/org/example/servlet/jenkins/SvetaServlet.java
@@ -0,0 +1,17 @@
+package org.example.servlet.jenkins;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class SvetaServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        PrintWriter writer = resp.getWriter();
+        String message = "Hello Sveta!";
+        writer.println(message);
+    }
+}
