c27b4e7ba4dd36df67ffeda5590358ac9da56af1
Sveta Piven <svetapiven93@icloud.com>
2023-08-29 12:33:17 +0300

    bug fix

--- src/main/java/org/example/servlet/jenkins/TestServlet.java
+++ src/main/java/org/example/servlet/jenkins/TestServlet.java
@@ -1,5 +1,6 @@
 package org.example.servlet.jenkins;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -8,10 +9,11 @@ import java.io.PrintWriter;
 
 public class TestServlet extends HttpServlet {
 
+    private final String message = "Hello!!!";
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter writer = resp.getWriter();
-        String message = "Hello Sveta!";
         writer.println(message);
     }
 }
